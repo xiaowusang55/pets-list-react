95723c645386eee9100874be79b63313962413bf
xiaowusang55 <xiaowusang@gmail.com>
2021-01-31 23:34:33 +0800

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  semi: true, // 结尾处冒号
+  bracketSpacing: true, // 括号处空格
+  trailingComma: 'es5', // 是否结尾有逗号
+  arrowParens: 'avoid', // 箭头函数参数是否带括号
+  singleQuote: true, // 使用单引号
+};
